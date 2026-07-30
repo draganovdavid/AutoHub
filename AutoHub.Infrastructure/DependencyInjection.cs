3e6d2a6714c6c7d717c230fbfd6334dbdb356490
David Draganov <daviddraganov891@gmail.com>
2026-07-30 15:57:13 +0300

    Add infrastructure DI extensions.

---
+++ AutoHub.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using AutoHub.Infrastructure.Persistence;
+using AutoHub.Infrastructure.Persistence.Interceptors;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoHub.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+
+            services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection")
+                    ?? throw new InvalidOperationException(
+                        "Connection string 'DefaultConnection' not found.");
+
+                options.UseSqlServer(connectionString);
+
+                options.AddInterceptors(
+                    serviceProvider.GetRequiredService<AuditableEntitySaveChangesInterceptor>());
+            });
+
+            return services;
+        }
+    }
+}
