97b9ad6851ed91fcce1b4815bdc2e0a034ff7805
David Draganov <daviddraganov891@gmail.com>
2026-07-31 17:33:51 +0300

    Adding Appilcation layer.

--- AutoHub.Api/Program.cs
+++ AutoHub.Api/Program.cs
@@ -1,9 +1,11 @@
 using AutoHub.Infrastructure;
+using AutoHub.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
