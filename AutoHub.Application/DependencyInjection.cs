0850959517faa50ee6cbdebb601229a574ebc186
David Draganov <daviddraganov891@gmail.com>
2026-08-08 14:40:59 +0300

    Registering services.

---
+++ AutoHub.Application/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using AutoHub.Application.Common.Behaviours;
+using AutoHub.Application.Common.Interfaces;
+using AutoHub.Application.Common.Services;
+using AutoHub.Application.Features;
+using AutoHub.Application.VehicleTypes;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AutoHub.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.AddSingleton(TimeProvider.System);
+
+            services.AddScoped(typeof(ILookupService<>), typeof(LookupService<>));
+            services.AddScoped<IVehicleTypeService, VehicleTypeService>();
+            services.AddScoped<IFeatureService, FeatureService>();
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+            });
+
+            return services;
+        }
+    }
+}
